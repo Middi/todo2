d3261a58a18870500acf6212cb3ec4194446963f
“Richard Middleton” <Richard@richardmiddleton.me>
2017-07-08 14:08:32 +0200

    create new tasks and remove new tasks

--- assets/js/main-min.js
+++ assets/js/main-min.js
@@ -1,1 +1,1 @@
-$("li").click(function(){$(this).toggleClass("completed")});
+$("ul").on("click","li",function(){$(this).toggleClass("completed")}),$("ul").on("click","span",function(t){$(this).parent().fadeOut(500,function(){$(this).remove()}),t.stopPropagation()}),$("input[type='text']").keypress(function(t){if(13===t.which){var i=$(this).val();$(this).val(""),$("ul").append("<li><span>X</span> "+i+"</li>")}});
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,23 @@
 // Check off todos by clicking
 
-$("li").click(function () { 
+$("ul").on("click", "li", function () { 
     $(this).toggleClass("completed");
+});
+
+// Clicking X removes Todo
+
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //Grabbing todo text from input
+        var todoInput = $(this).val();
+        $(this).val("");
+        $("ul").append('<li><span>X</span> ' + todoInput + '</li>');
+    }
 });
